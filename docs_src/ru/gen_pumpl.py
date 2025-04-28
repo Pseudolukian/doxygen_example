d65da5a780693f3f5be81fd8b07ae0049dcb67e6
Pseudolukian <pynanist@gmail.com>
2025-04-28 13:37:41 +0200

    Finish writing do

--- docs_src/ru/gen_pumpl.py
+++ docs_src/ru/gen_pumpl.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import subprocess
+import re
 
 root = "../../"
 docs_src = root + "docs_src/ru/"
@@ -12,11 +13,15 @@ file_contents = {
 }
 
 def generate_plantuml_diagrams(file_dict):
+    i = 0
     for file_path, lines in file_dict.items():
-        for line in lines:
-            print(line)
-            subprocess.run(f"hpp2plantuml -i {line} -o {file_path }", shell=True)
-
         
+        for line in lines:
+            dir_name = Path(file_path).parent.name
+            base_output_path = Path(file_path).parent / f"class_{dir_name}.puml"
+            print(dir_name)
+            #print(base_output_path)
+            #subprocess.run(f"hpp2plantuml -i {line} -o {file_path }", shell=True)
+
 
 generate_plantuml_diagrams(file_contents)
